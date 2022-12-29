9b95386363214b8c682e1d92007eb1a9ccab9a8d
Saugat2002 <102962967+Saugat2002@users.noreply.github.com>
2022-12-29 19:32:59 +0545

    Created

---
+++ index.js
@@ -0,0 +1,15 @@
+const dogImageDiv = document.getElementById("dogImage");
+const getNewDogDiv = document.getElementById("getNewDog");
+
+const fetchDog = () => {
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json.message);
+      dogImageDiv.innerHTML = `<img src=${json.message} alt="DogImage">`;
+    });
+};
+
+getNewDogDiv.onclick = () => {
+    fetchDog();
+};
